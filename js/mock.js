d3c05a4c97954fc8497bd769633f79577da9a565
bruno.oliveira <brunotco4@gmail.com>
2023-08-14 10:34:27 +0100

    mock data just for showing

---
+++ js/mock.js
@@ -0,0 +1,27 @@
+const Products = [
+    {
+        name: "Laptop",
+        price: 1299,
+        quantity: 3
+    },
+    {
+        name: "Smartphone",
+        price: 599,
+        quantity: 10
+    },
+    {
+        name: "Smartwatch",
+        price: 89,
+        quantity: 6
+    },
+    {
+        name: "Desktop",
+        price: 2199,
+        quantity: 1
+    },
+    {
+        name: "Headset",
+        price: 29,
+        quantity: 0
+    }
+]
