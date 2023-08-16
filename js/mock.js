c1d93653a5b4045ba3a88a9dffbde317324340b0
bruno.oliveira <brunotco4@gmail.com>
2023-08-16 09:31:59 +0100

    added part number to products

--- js/mock.js
+++ js/mock.js
@@ -1,26 +1,31 @@
 const Products = [
     {
         name: "Laptop",
+        number: 61997,
         price: 1299,
         quantity: 3
     },
     {
         name: "Smartphone",
+        number: 31857,
         price: 599,
         quantity: 10
     },
     {
         name: "Smartwatch",
+        number: 72997,
         price: 89,
         quantity: 6
     },
     {
         name: "Desktop",
+        number: 10614,
         price: 2199,
         quantity: 1
     },
     {
         name: "Headset",
+        number: 15665,
         price: 29,
         quantity: 0
     }
